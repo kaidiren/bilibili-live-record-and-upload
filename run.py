48afb87d855cb8fc27a4711bc1a707e326b9a262
kaidiren <kaidiren@gmail.com>
2019-03-08 09:46:49 +0800

    upload

--- run.py
+++ run.py
@@ -11,32 +11,6 @@ urllib3.disable_warnings()
 
 import atexit
 
-@atexit.register
-def upload():
-    b = Bilibili()
-    b.login(config.username, config.password)
-    print('正在上传')
-    videos = [f for f in os.listdir('files') if f.endswith('.flv')]
-
-    tag = ['陈哥', '陈哥1', '直播', '录播']
-
-    desc = '陈哥404直播录播, 服务器自动录播自动投稿 喜欢的可以去关注陈哥1 https://live.bilibili.com/404'
-
-    for video in videos:
-        filepath = os.path.abspath('./files/' + video)
-        stat = os.stat(filepath)
-        if stat.st_size <= 200 * 1024 * 1024:
-            os.rename(filepath, filepath + '.skip')
-            continue
-
-        title = config.title_prefix + os.path.basename(filepath)[:-4]
-        tid = 17
-        b.upload(VideoPart(filepath), title, tid, tag, desc)
-        os.rename(filepath, filepath + '.uploaded')
-        time.sleep(60)
-
-    print('上传成功')
-
 class BiliBiliLiveRecorder(BiliBiliLive):
     def __init__(self, room_id):
         super().__init__(room_id)
@@ -77,7 +51,30 @@ class BiliBiliLiveRecorder(BiliBiliLive):
             c_filename = os.path.join(os.getcwd(), 'files', filename)
             self.record(urls[0], c_filename)
             self.print(self.room_id, '录制完成')
-            upload()
+            self.upload(c_filename)
+    def upload(self, filepath):
+        b = Bilibili()
+        b.login(config.username, config.password)
+        print('正在上传', filepath)
+
+        tag = ['陈哥', '陈哥1', '直播', '录播']
+
+        desc = '陈哥404直播录播, 服务器自动录播自动投稿 喜欢的可以去关注陈哥1 https://live.bilibili.com/404'
+
+        stat = os.stat(filepath)
+        if stat.st_size <= 20 * 1024 * 1024:
+            os.rename(filepath, filepath + '.skip')
+            return
+
+        title = config.title_prefix + os.path.basename(filepath)[:-4]
+        tid = 17
+        
+        b.upload(VideoPart(filepath), title, tid, tag, desc)
+        os.rename(filepath, filepath + '.uploaded')
+        
+        time.sleep(60)
+
+        print('上传成功', filepath)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
---
+++ upload.py
@@ -0,0 +1,27 @@
+
+import config
+from bilibiliupload import *
+
+b = Bilibili()
+b.login(config.username, config.password)
+print('正在上传')
+videos = [f for f in os.listdir('files') if f.endswith('.flv')]
+
+tag = ['陈哥', '陈哥1', '直播', '录播']
+
+desc = '陈哥404直播录播, 服务器自动录播自动投稿 喜欢的可以去关注陈哥1 https://live.bilibili.com/404'
+
+for video in videos:
+    filepath = os.path.abspath('./files/' + video)
+    stat = os.stat(filepath)
+    if stat.st_size <= 200 * 1024 * 1024:
+        os.rename(filepath, filepath + '.skip')
+        continue
+
+    title = config.title_prefix + os.path.basename(filepath)[:-4]
+    tid = 17
+    b.upload(VideoPart(filepath), title, tid, tag, desc)
+    os.rename(filepath, filepath + '.uploaded')
+    time.sleep(60)
+
+print('上传成功')

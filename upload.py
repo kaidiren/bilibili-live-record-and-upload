ed375e512e041da87e5a0520de0f2a87c4b592ea
kaidiren <kaidiren@gmail.com>
2019-04-03 17:08:00 +0800

    update logic

--- upload.py
+++ upload.py
@@ -8,26 +8,16 @@ from datetime import datetime, timedelta
 
 from bilibiliupload import Bilibili, VideoPart
 
-
 if os.path.isfile('files/.recording'):
     print('.recording exists')
     sys.exit()
 
-if os.path.isfile('files/.uploading'):
-    if time.time() - os.stat('files/.uploading').st_mtime > 3600 * 4:
-        requests.get(url=config.pushbear, params={
-            'text': '上传可能出BUG了',
-            'desp': '上传可能出BUG了',
-            'sendkey': config.sendkey
-        })
-    sys.exit()
+delta = 1
+for arg in sys.argv:
+    if arg == '-t':
+        delta = 0
 
-open('files/.uploading', 'a').close()
-
-desc = '[已授权]陈哥404直播录播, 仅用于回看，请勿商业使用 请关注陈哥直播间 https://live.bilibili.com/404\n服务器每天自动录播自动投稿，文件过大自动分P，凌晨2点定时上传，审稿需要3-6小时请耐心等待\n本录播不做整理，喜欢看整理过的录播可以去空间 11693477 观看，催更毫无意义，谢谢~'
-
-
-yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y%m%d')
+yesterday = datetime.strftime(datetime.now() - timedelta(delta), '%Y%m%d')
 sub_dir = 'files/' + yesterday + '/'
 
 if not os.path.exists(sub_dir):
@@ -37,11 +27,26 @@ if os.path.isfile(sub_dir + '.uploaded'):
     print('.uploaded file exists')
     sys.exit()
 
+
+if os.path.isfile(sub_dir + '.uploading'):
+    if time.time() - os.stat(sub_dir + '.uploading').st_mtime > 3600 * 4:
+        requests.get(url=config.pushbear, params={
+            'text': '上传可能出BUG了',
+            'desp': '上传可能出BUG了',
+            'sendkey': config.sendkey
+        })
+    sys.exit()
+
+open(sub_dir + '.uploading', 'a').close()
+
+desc = '[已授权]陈哥404直播录播, 仅用于回看，请勿商业使用 请关注陈哥直播间 https://live.bilibili.com/404\n服务器每天自动录播自动投稿，文件过大自动分P，凌晨2点定时上传，审稿需要3-6小时请耐心等待\n本录播不做整理，喜欢看整理过的录播可以去空间 11693477 观看，催更毫无意义，谢谢~'
+
 videos = [f for f in os.listdir(sub_dir) if f.endswith('.flv')]
 videos.sort()
 
 if not len(videos):
-   print('no living yesterday')
+   print('no living yesterday: ' + sub_dir)
+   os.remove(sub_dir + '.uploading') if os.path.exists(sub_dir + '.uploading') else None
    sys.exit()
 
 tag = ['陈哥404直播录播', '无情服务器录播', '404录播姬']
@@ -72,4 +77,4 @@ for dir in dirs:
         shutil.rmtree(dirpath)
         print(dirpath, 'deleted')
 
-os.remove('files/.uploading') if os.path.exists('files/.uploading') else None
+os.remove(sub_dir + '.uploading') if os.path.exists(sub_dir + '.uploading') else None
